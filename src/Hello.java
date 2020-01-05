772c07d98797b6419cf4a6a72b01da64053e02b8
namilan <nichole.milan@gmail.com>
2020-01-04 19:07:44 -0500

    update

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,19 @@ public class Hello {
 	
 	public static void main(String[] args) {
 	
-		System.out.println("Hello World");
+		System.out.println("Hello, Nichole ");
+		
+			int myFirstNumber = (10 + 5) + (2 * 10);
+			System.out.println(myFirstNumber);
+			
+			int mySecondNumber = 12;
+			System.out.println(mySecondNumber);
+			
+			int myThirdNumber = 6;
+			System.out.println(myThirdNumber);
+			
+			int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+			System.out.println(myTotal);
 	}
 
 }
