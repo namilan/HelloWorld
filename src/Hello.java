92f27183e332caff01c777b287b4310bc407b0b0
namilan <nichole.milan@gmail.com>
2020-01-04 19:12:06 -0500

    update

--- src/Hello.java
+++ src/Hello.java
@@ -16,6 +16,9 @@ public class Hello {
 			
 			int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 			System.out.println(myTotal);
+			
+			int myLastOne = 1000 - myTotal;
+			System.out.println(myLastOne);
 	}
 
 }
